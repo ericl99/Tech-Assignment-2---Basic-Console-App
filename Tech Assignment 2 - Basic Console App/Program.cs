b88059124a95adecb744705765c7897291264021
Erick Leon <ericl1@usf.edu>
2022-01-28 09:12:55 -0500

    Add project files.

---
+++ Tech Assignment 2 - Basic Console App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+// Erick Leon
+namespace Tech_Assignment_2___Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, my name is Erick Leon and I am a senior at USF posting my first assignemnt for ISM4300!");
+        }
+    }
+}
